72bfc29dbf9a092c8e4ef68b177a894f44cdc697
Vladislav Korotkov <slavakorr@gmail.com>
2018-05-26 15:59:30 +0300

    implement `is_a`, `is_not_a` methods

--- solutions/ruby/builder_of_things/26052018/thing.rb
+++ solutions/ruby/builder_of_things/26052018/thing.rb
@@ -1,2 +1,40 @@
 class Thing
+  attr_reader :name, :context, :parent
+  def initialize(name, context = nil, parent = nil)
+     @name = name
+     @context = context
+     @parent = parent
+     @values = {}
+  end
+
+  def method_missing(name, *args)
+    case name
+    when :is_a
+      create_new_thing_with_value(true)
+    when :is_not_a
+      create_new_thing_with_value(false)
+    else
+      if name.to_s.end_with?('?')
+        read_value(name.to_s.delete('?'))
+      else
+        send_value_to_parent(parent, name.to_s, context)
+      end
+    end
+  end
+
+  def create_new_thing_with_value(context)
+    Thing.new(name, context, self)
+  end
+
+  def send_value_to_parent(parent, name, context)
+    parent.set_value(name, context)
+  end
+
+  def set_value(name, context)
+    @values[name] = context
+  end
+
+  def read_value(name)
+    @values[name]
+  end
 end
